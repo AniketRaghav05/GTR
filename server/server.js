bed9f20fdb708d726a9be235c0f4614c4c86a537
Aniket Raghav <aniketraghav05@gmail.com>
2026-05-11 03:01:26 +0530

    Fix formatting issues in server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+
 const mongoose = require("mongoose");
+
 const cors = require("cors");
 
 require("dotenv").config();
@@ -62,13 +64,13 @@ mongoose
     const PORT =
       process.env.PORT || 5000;
 
-      app.listen(PORT, () => {
+    app.listen(PORT, () => {
 
       console.log(
         `Server running on port ${PORT}`
       );
 
-  });
+    });
 
   })
 
